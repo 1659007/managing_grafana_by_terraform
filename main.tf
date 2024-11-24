47bc261c5b4909e9731bd2279d968a24fb96c2f6
voquangduy <34533098+1659007@users.noreply.github.com>
2024-11-24 11:53:36 +0700

    create new file to configuration Grafana Oncall - update oncall token

--- main.tf
+++ main.tf
@@ -17,6 +17,6 @@ provider "grafana" {
 provider "grafana" {
    alias = "oncall"
 
-   oncall_access_token = "f74b0669bd7cdfa1b76e5e50a40717d773e5aa220e82c72aee8fda5bf828"
+   oncall_access_token = "738ad693b1ddbc1cff1cd670102d5457bada34562bb699638b4251bec7d49d8a"
    oncall_url = "https://oncall-prod-us-central-0.grafana.net/oncall/"
 }
