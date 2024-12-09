d41051d61a9de420d31342b8f36ef4d5fc7ca2a5
voquangduy <34533098+1659007@users.noreply.github.com>
2024-12-09 20:41:17 +0700

    Update cicd alerts

--- data-source.tf
+++
@@ -1,6 +0,0 @@
-# resource "grafana_data_source" "grafanaTestdataDatasource" {
-#   provider = grafana.cloud
-#
-#   name = var.datasource
-#   type = var.datasource
-# }
--- variable.tf
+++ variable.tf
@@ -1,14 +1,14 @@
-# variable "folder_uid" {
-#   type      = string
-#   nullable = false
-# }
-#
-# variable "alert_uid" {
-#   type      = string
-#   nullable = false
-# }
-#
-# variable "datasource" {
-#   type      = string
-#   nullable = false
-# }
+variable "folder_uid" {
+  type      = string
+  nullable = false
+}
+
+variable "alert_uid" {
+  type      = string
+  nullable = false
+}
+
+variable "datasource" {
+  type      = string
+  nullable = false
+}
